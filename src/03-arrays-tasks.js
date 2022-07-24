cab76558021b2f906349cb2923416d5dcd95dcce
Marina Elizarova <seagirl1110@gmail.com>
2022-07-24 16:05:10 +0300

    fix: add solution arrays-tasks

--- src/03-arrays-tasks.js
+++ src/03-arrays-tasks.js
@@ -35,8 +35,8 @@ function findElement(arr, value) {
  *    2 => [ 1, 3 ]
  *    5 => [ 1, 3, 5, 7, 9 ]
  */
-function generateOdds(/* len */) {
-  throw new Error('Not implemented');
+function generateOdds(len) {
+  return Array.from(Array(len * 2), (item, index) => index).filter((item) => item % 2 !== 0);
 }
 
 
